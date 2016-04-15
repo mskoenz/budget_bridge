8a18db5fdf6bd95d1ac84046a9d02e4e5959b8d2
mskoenz <mskoenz@gmx.net>
2016-04-15 13:15:12 +0200

    02.Commit:	added gitignore mskoenz
    DONE:
    -
    hash-word: fine range

--- budget_bridge.cpp
+++ budget_bridge.cpp
@@ -9,6 +9,7 @@
  ******************************************************************************/
 
 #include <iostream>
+#include <fstream>
 #include <fsc/ArgParser.hpp>
 
 int main(int argc, char * argv[]) {
@@ -23,8 +24,41 @@ int main(int argc, char * argv[]) {
     double energy = ap["energy"];
     double velocity = ap["velocity"];
     
-    std::cout << energy << std::endl;
-    std::cout << velocity << std::endl;
+    if(ap.freeargc() == 2) {
+        std::vector<double> a[3];
+        for(uint32_t n = 0; n < 3; ++n) {
+            a[n].resize(9);
+        }
+        
+        std::string in;
+        
+        for(uint32_t n = 0; n < 2; ++n) {
+            std::string filename = ap[n];
+            std::ifstream is(filename);
+            
+            if(is.is_open())
+                for(uint32_t i = 0; i < 9; ++i) {
+                    is >> in;
+                    a[n][i] = std::stoi(in);
+                }
+            is.close();
+        }
+        
+        std::ofstream os("res.txt");
+        for(uint32_t i = 0; i < 9; ++i) {
+            a[2][i] = (a[1][i] - a[0][i])*energy - velocity;
+            os << a[2][i] << "\n";
+        }
+        os.close();
+        
+        
+        
+        
+    } else {
+        std::cout << energy << std::endl;
+        std::cout << velocity << std::endl;
+    }
+    
     
     return 0;
 }
